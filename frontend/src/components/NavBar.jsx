ce53ab69413d1d2a08f408bac19af6d8cd684e9c
QASIM-LOKHANDWALA <qasimlokhandwala1@gmail.com>
2025-03-05 11:00:00 +0530

    Initialize Frontend

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,47 @@
+import { FaSquarePlus } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+    return (
+        <nav
+            className="navbar bg-body-tertiary py-50"
+            style={{ padding: "20px" }}
+        >
+            <div className="container d-flex justify-content-around">
+                <a className="navbar-brand" href="/">
+                    <h4 style={{ fontWeight: "semibold" }}>DailyRecord</h4>
+                </a>
+                <div className="d-flex">
+                    <div
+                        className="input-group input-group-sm"
+                        style={{ width: "500px", height: "40px" }}
+                    >
+                        <input
+                            className="form-control"
+                            type="search"
+                            placeholder="Search"
+                            aria-label="Search"
+                        />
+                        <button
+                            className="btn btn-outline-success"
+                            type="submit"
+                        >
+                            Search
+                        </button>
+                    </div>
+                </div>
+
+                <a href="/add-notes" style={{ textDecoration: "none" }}>
+                    <button
+                        className="btn btn-outline-primary btn-md"
+                        type="button"
+                    >
+                        <FaSquarePlus /> Add Notes
+                    </button>
+                </a>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
