2b0a5796297ef2d25de81e61039938b189a9f8b9
QASIM-LOKHANDWALA <qasimlokhandwala1@gmail.com>
2025-03-05 09:22:35 +0530

    Implement GET, PUT and DELETE request for note

--- backend/noteapp/urls.py
+++ backend/noteapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', view=views.notesView, name='notes')
+    path('', view=views.notesView, name='notes'),
+    path('<str:slug>/', view=views.noteDetailView, name='note-detail'),
 ]
--- backend/noteapp/views.py
+++ backend/noteapp/views.py
@@ -14,6 +14,7 @@ def notesView(request):
         notes = Note.objects.all()
         serializer = NoteSerializer(notes, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
     elif request.method == 'POST':
         serializer = NoteSerializer(data=request.data)
         if serializer.is_valid():
@@ -21,3 +22,26 @@ def notesView(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         print(f'***************\n{serializer.errors}\n***************')
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def noteDetailView(request, slug):
+    try:
+        note = Note.objects.get(slug=slug)
+    except Note.DoesNotExist as e:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = NoteSerializer(note)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'PUT':
+        serializer = NoteSerializer(note, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        print(f'***************\n{serializer.errors}\n***************')
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        note.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
